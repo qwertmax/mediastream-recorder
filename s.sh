93903b9be2b9eae8d4cbb48bec45663ead17de66
Maxim Tishchenko <qwertmax@gmail.com>
2020-06-19 16:05:41 +0300

    docker run

--- s.sh
+++ s.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --rm -it -p 0.0.0.0:80:80 -v $(pwd):/usr/local/app -w /usr/local/app  node:14 bash
+docker run --rm -it -p 0.0.0.0:443:443 -v $(pwd):/usr/local/app -w /usr/local/app  node:14 bash
